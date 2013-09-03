e3004ba1cc3d0446827f99b541e4b86b9633d22b
Jerry Yuan <Jerry.Yuan@ontario.ca>
2013-09-03 13:54:11 -0400

    remove unrequired files

--- TRAIS/minify.bat
+++
@@ -1,2 +0,0 @@
-java -jar ../MOEMap/build/yuicompressor-2.4.7.jar TRAIS_Report.js -o Y:/web/TRAIS/TRAIS_Report-min.js
-java -jar ../MOEMap/build/yuicompressor-2.4.7.jar oda.js -o Y:/web/TRAIS/oda-min.js
