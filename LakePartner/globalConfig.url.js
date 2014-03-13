f95646f3669931c4b8f33923ebfdae8b7db6f7eb
Jerry Yuan <yhyuan@gmail.com>
2014-03-13 16:37:31 -0400

    Fix bugs

--- LakePartner/globalConfig.url.js
+++ LakePartner/globalConfig.url.js
@@ -1,3 +1,4 @@
-/*globalConfig.url = "http://lrcdrrvsdvap002/ArcGIS/rest/services/Interactive_Map_Public/LakePartner/MapServer";*/
+globalConfig.url = "http://lrcdrrvsdvap002/ArcGIS/rest/services/Interactive_Map_Public/LakePartner/MapServer";
 
-globalConfig.url = "http://www.appliomaps.lrc.gov.on.ca/ArcGIS/rest/services/MOE/lakepartner/MapServer";
+//globalConfig.url = "http://www.appliomaps.lrc.gov.on.ca/ArcGIS/rest/services/MOE/lakepartner/MapServer";
+
