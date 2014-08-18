d16a2eb91310ea6c78ba301118df133d2185e69b
Jerry Yuan <yhyuan@gmail.com>
2014-08-18 12:40:27 -0400

    DrinkingWater DWSP

--- DrinkingWater/build_TasteOdour_Report_fr.bat
+++ DrinkingWater/build_TasteOdour_Report_fr.bat
@@ -1,7 +1,7 @@
 ant -f build_TasteOdour_Report_fr.xml
-rm TasteOdour_Report_en.2.0-concatenate.js
-rm TasteOdour_Report_en.2.0-concatenate-min.js
-cp TasteOdour_Report_en.2.0-min.js /Y/web/DrinkingWater/fr
-rm TasteOdour_Report_en.2.0-min.js
-cp TasteOdour_Report_en.htm /Y/web/DrinkingWater/fr
-mv /Y/web/DrinkingWater/fr/TasteOdour_Report_en.htm  /Y/web/DrinkingWater/fr/TasteOdour_Report.htm
+rm TasteOdour_Report_fr.2.0-concatenate.js
+rm TasteOdour_Report_fr.2.0-concatenate-min.js
+cp TasteOdour_Report_fr.2.0-min.js /Y/web/DrinkingWater/fr
+rm TasteOdour_Report_fr.2.0-min.js
+cp TasteOdour_Report_fr.htm /Y/web/DrinkingWater/fr
+mv /Y/web/DrinkingWater/fr/TasteOdour_Report_fr.htm  /Y/web/DrinkingWater/fr/TasteOdour_Report.htm
