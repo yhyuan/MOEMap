6a530813bb67101159fac08648a8fd2de4f6e9e8
Jerry Yuan <yhyuan@gmail.com>
2016-01-13 15:04:19 -0500

    Wells

--- Wells/globalConfig.url.js
+++ Wells/globalConfig.url.js
@@ -1,5 +1,5 @@
 //globalConfig.url = "http://lrcdrrvsdvap002/ArcGIS/rest/services/Interactive_Map_Public/Wells1/MapServer";
 //globalConfig.url = "http://LRCTPTVSUAAP004/ArcGIS/rest/services/Interactive_Map_Public/Wells/MapServer";
 //globalConfig.url = "http://www.appliomaps.lrc.gov.on.ca/ArcGIS/rest/services/MOE/Wells/MapServer";   //PROD in LRC
-globalConfig.url = "http://www.appliomapss.lrc.gov.on.ca/ArcGIS/rest/services/MOE/Wells/MapServer";  //UAT in LRC
+globalConfig.url = "https://www.giscoeservices.lrc.gov.on.ca/ArcGIS/rest/services/MOE/Wells/MapServer";  //UAT in LRC
 
